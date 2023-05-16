9e3a92a2ad894e73b8013e6c416ebea604d770fb
Michael Wu <wuxiaohua1011@gmail.com>
2023-05-15 17:45:43 -0700

    lite: making sure things build

--- src/agent/src/planning/local_planning/a_star/include/a_star/a_star.hpp
+++ src/agent/src/planning/local_planning/a_star/include/a_star/a_star.hpp
@@ -2,31 +2,69 @@
 #define A_STAR_HPP_
 #include "trajectory_generator/trajectory_generator_interface.hpp"
 #include <memory>
+
+/* start of migrated imports from smac planner */
+#include "smac_planner/a_star.hpp"
+#include "smac_planner/smoother.hpp"
+#include "smac_planner/costmap_downsampler.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav2_core/global_planner.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
+#include "nav2_costmap_2d/costmap_2d.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav2_util/lifecycle_node.hpp"
+#include "nav2_util/node_utils.hpp"
+#include "tf2/utils.h"
+/* end of migrated imports from smac planner */
+#include <smac_planner/types.hpp>
+#include <smac_planner/constants.hpp>
+
 namespace local_planning
 {
-    class AStar : public TrajectoryGeneratorInterface
-    {
-    public:
-    AStar(){
-        this->name = "A* algo";
-    } 
-    nav_msgs::msg::Path
-    computeTrajectory(const nav2_msgs::msg::Costmap::SharedPtr costmap,
-                        const nav_msgs::msg::Odometry::SharedPtr odom,
-                        const geometry_msgs::msg::PoseStamped::SharedPtr next_waypoint) override;
+class AStar : public TrajectoryGeneratorInterface
+{
+public:
+  AStar()
+  {
+    this->name = "A* algo";
+  }
 
-    void configure(rclcpp_lifecycle::LifecycleNode * parent, 
-                    std::shared_ptr<tf2_ros::Buffer> tf) override ;
+  void p_bridgeSMACConfigure();
 
-    void cleanup() override; 
+  nav_msgs::msg::Path computeTrajectory(const nav2_msgs::msg::Costmap::SharedPtr costmap,
+                                        const nav_msgs::msg::Odometry::SharedPtr odom,
+                                        const geometry_msgs::msg::PoseStamped::SharedPtr next_waypoint) override;
 
-    void activate() override;
+  void configure(rclcpp_lifecycle::LifecycleNode* parent, std::shared_ptr<tf2_ros::Buffer> tf) override;
 
-    void deactivate();
+  void cleanup() override;
 
-    private:
-            rclcpp_lifecycle::LifecycleNode * parent_node;
-            std::shared_ptr<tf2_ros::Buffer> tf_buffer;
-    };
-}
-#endif // A_STAR_HPP_
+  void activate() override;
+
+  void deactivate();
+
+private:
+  rclcpp_lifecycle::LifecycleNode* parent_node;
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer;
+
+protected:
+  std::unique_ptr<smac_planner::AStarAlgorithm<smac_planner::NodeSE2>> _a_star;
+  std::unique_ptr<smac_planner::Smoother> _smoother;
+  rclcpp::Clock::SharedPtr _clock;
+  rclcpp::Logger _logger{rclcpp::get_logger("SmacPlanner")};
+  nav2_costmap_2d::Costmap2D * _costmap;
+  std::unique_ptr<smac_planner::CostmapDownsampler> _costmap_downsampler;
+  std::string _global_frame, _name;
+  float _tolerance;
+  int _downsampling_factor;
+  unsigned int _angle_quantizations;
+  double _angle_bin_size;
+  bool _downsample_costmap;
+  rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>::SharedPtr _raw_plan_publisher;
+  smac_planner::SmootherParams _smoother_params;
+  smac_planner::OptimizerParams _optimizer_params;
+  double _max_planning_time;
+};
+}  // namespace local_planning
+#endif  // A_STAR_HPP_
--- src/agent/src/planning/local_planning/a_star/src/a_star.cpp
+++ src/agent/src/planning/local_planning/a_star/src/a_star.cpp
@@ -3,46 +3,153 @@
 
 namespace local_planning
 {
-    void AStar::configure(rclcpp_lifecycle::LifecycleNode * parent,
-                          std::shared_ptr<tf2_ros::Buffer> tf)
-    {
-        this->parent_node = parent;
-        this->tf_buffer = tf;
+void AStar::configure(rclcpp_lifecycle::LifecycleNode* parent, std::shared_ptr<tf2_ros::Buffer> tf)
+{
+  this->parent_node = parent;
+  this->tf_buffer = tf;
+  this->name = name;
 
-        this->name = name;
+  p_bridgeSMACConfigure();
 
-        RCLCPP_INFO(rclcpp::get_logger(this->name), "Configured");
-
-    }
-
-    void AStar::cleanup()
-    {
-        this->parent_node = nullptr;
-        this->tf_buffer = nullptr;
-        RCLCPP_INFO(rclcpp::get_logger(this->name), "cleanup");
-
-    }
-
-    void AStar::activate()
-    {
-        // Implementation details for activation
-        RCLCPP_INFO(rclcpp::get_logger(this->name), "activate");
-
-    }
-
-    void AStar::deactivate()
-    {
-        // Implementation details for deactivation
-        RCLCPP_INFO(rclcpp::get_logger(this->name), "deactivate");
-    }
-
-    nav_msgs::msg::Path AStar::computeTrajectory(const nav2_msgs::msg::Costmap::SharedPtr costmap,
-                                                 const nav_msgs::msg::Odometry::SharedPtr odom,
-                                                 const geometry_msgs::msg::PoseStamped::SharedPtr next_waypoint)
-    {
-        nav_msgs::msg::Path path;
-        // Implementation details for trajectory computation using A*
-        return path;
-    }
+  RCLCPP_INFO(rclcpp::get_logger(this->name), "Configured");
 }
 
+void AStar::cleanup()
+{
+  this->parent_node = nullptr;
+  this->tf_buffer = nullptr;
+  RCLCPP_INFO(rclcpp::get_logger(this->name), "cleanup");
+}
+
+void AStar::activate()
+{
+  // Implementation details for activation
+  RCLCPP_INFO(rclcpp::get_logger(this->name), "activate");
+}
+
+void AStar::deactivate()
+{
+  // Implementation details for deactivation
+  RCLCPP_INFO(rclcpp::get_logger(this->name), "deactivate");
+}
+
+void AStar::p_bridgeSMACConfigure()
+{
+  bool allow_unknown;
+  int max_iterations;
+  int max_on_approach_iterations = std::numeric_limits<int>::max();
+  int angle_quantizations;
+  smac_planner::SearchInfo search_info;
+  bool smooth_path;
+  std::string motion_model_for_search;
+
+  // General planner params
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".tolerance", rclcpp::ParameterValue(0.125));
+  _tolerance = static_cast<float>(this->parent_node->get_parameter(name + ".tolerance").as_double());
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".downsample_costmap", rclcpp::ParameterValue(false));
+  this->parent_node->get_parameter(name + ".downsample_costmap", _downsample_costmap);
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".downsampling_factor", rclcpp::ParameterValue(1));
+  this->parent_node->get_parameter(name + ".downsampling_factor", _downsampling_factor);
+
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".angle_quantization_bins", rclcpp::ParameterValue(72));
+  this->parent_node->get_parameter(name + ".angle_quantization_bins", angle_quantizations);
+  _angle_bin_size = 2.0 * M_PI / angle_quantizations;
+  _angle_quantizations = static_cast<unsigned int>(angle_quantizations);
+
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".allow_unknown", rclcpp::ParameterValue(true));
+  this->parent_node->get_parameter(name + ".allow_unknown", allow_unknown);
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".max_iterations", rclcpp::ParameterValue(-1));
+  this->parent_node->get_parameter(name + ".max_iterations", max_iterations);
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".smooth_path", rclcpp::ParameterValue(false));
+  this->parent_node->get_parameter(name + ".smooth_path", smooth_path);
+
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".minimum_turning_radius", rclcpp::ParameterValue(0.2));
+  this->parent_node->get_parameter(name + ".minimum_turning_radius", search_info.minimum_turning_radius);
+
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".reverse_penalty", rclcpp::ParameterValue(2.0));
+  this->parent_node->get_parameter(name + ".reverse_penalty", search_info.reverse_penalty);
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".change_penalty", rclcpp::ParameterValue(0.5));
+  this->parent_node->get_parameter(name + ".change_penalty", search_info.change_penalty);
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".non_straight_penalty", rclcpp::ParameterValue(1.05));
+  this->parent_node->get_parameter(name + ".non_straight_penalty", search_info.non_straight_penalty);
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".cost_penalty", rclcpp::ParameterValue(1.2));
+  this->parent_node->get_parameter(name + ".cost_penalty", search_info.cost_penalty);
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".analytic_expansion_ratio", rclcpp::ParameterValue(2.0));
+  this->parent_node->get_parameter(name + ".analytic_expansion_ratio", search_info.analytic_expansion_ratio);
+
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".max_planning_time_ms", rclcpp::ParameterValue(5000.0));
+  this->parent_node->get_parameter(name + ".max_planning_time_ms", _max_planning_time);
+
+  nav2_util::declare_parameter_if_not_declared(this->parent_node, name + ".motion_model_for_search",
+                                               rclcpp::ParameterValue(std::string("DUBIN")));
+  this->parent_node->get_parameter(name + ".motion_model_for_search", motion_model_for_search);
+  smac_planner::MotionModel motion_model = smac_planner::fromString(motion_model_for_search);
+  if (motion_model == smac_planner::MotionModel::UNKNOWN)
+  {
+    RCLCPP_WARN(rclcpp::get_logger(this->name),
+                "Unable to get MotionModel search type. Given '%s', "
+                "valid options are MOORE, VON_NEUMANN, DUBIN, REEDS_SHEPP.",
+                motion_model_for_search.c_str());
+  }
+
+  if (max_on_approach_iterations <= 0)
+  {
+    RCLCPP_INFO(rclcpp::get_logger(this->name),
+                "On approach iteration selected as <= 0, "
+                "disabling tolerance and on approach iterations.");
+    max_on_approach_iterations = std::numeric_limits<int>::max();
+  }
+
+  if (max_iterations <= 0)
+  {
+    RCLCPP_INFO(rclcpp::get_logger(this->name),
+                "maximum iteration selected as <= 0, "
+                "disabling maximum iterations.");
+    max_iterations = std::numeric_limits<int>::max();
+  }
+
+  // convert to grid coordinates
+  const double minimum_turning_radius_global_coords = search_info.minimum_turning_radius;
+  search_info.minimum_turning_radius =
+      search_info.minimum_turning_radius / (_costmap->getResolution() * _downsampling_factor);
+
+  _a_star = std::make_unique<smac_planner::AStarAlgorithm<smac_planner::NodeSE2>>(motion_model, search_info);
+  _a_star->initialize(allow_unknown, max_iterations, max_on_approach_iterations);
+//   _a_star->setFootprint(costmap_ros->getRobotFootprint(), costmap_ros->getUseRadius());
+
+  if (smooth_path)
+  {
+    _smoother = std::make_unique<smac_planner::Smoother>();
+    _optimizer_params.get(this->parent_node, name);
+    _smoother_params.get(this->parent_node, name);
+    _smoother_params.max_curvature = 1.0f / minimum_turning_radius_global_coords;
+    _smoother->initialize(_optimizer_params);
+  }
+
+  if (_downsample_costmap && _downsampling_factor > 1)
+  {
+    std::string topic_name = "downsampled_costmap";
+    _costmap_downsampler = std::make_unique<smac_planner::CostmapDownsampler>();
+    // _costmap_downsampler->on_configure(this->parent_node, _global_frame, topic_name, _costmap, _downsampling_factor);
+  }
+
+  _raw_plan_publisher = this->parent_node->create_publisher<nav_msgs::msg::Path>("unsmoothed_plan", 1);
+
+  RCLCPP_INFO(rclcpp::get_logger(this->name),
+              "Configured plugin %s of type SmacPlanner with "
+              "tolerance %.2f, maximum iterations %i, "
+              "max on approach iterations %i, and %s. Using motion model: %s.",
+              _name.c_str(), _tolerance, max_iterations, max_on_approach_iterations,
+              allow_unknown ? "allowing unknown traversal" : "not allowing unknown traversal",
+              toString(motion_model).c_str());
+}
+
+nav_msgs::msg::Path AStar::computeTrajectory(const nav2_msgs::msg::Costmap::SharedPtr costmap,
+                                             const nav_msgs::msg::Odometry::SharedPtr odom,
+                                             const geometry_msgs::msg::PoseStamped::SharedPtr next_waypoint)
+{
+  nav_msgs::msg::Path path;
+  // Implementation details for trajectory computation using A*
+  return path;
+}
+}  // namespace local_planning
