ccc47cbe026bea96dd1b5c0fb9d6f294016bad15
Michael Wu <wuxiaohua1011@gmail.com>
2023-05-15 23:33:29 -0700

    it runs now

--- src/agent/src/planning/local_planning/a_star/include/a_star/a_star.hpp
+++ src/agent/src/planning/local_planning/a_star/include/a_star/a_star.hpp
@@ -4,67 +4,79 @@
 #include <memory>
 
 /* start of migrated imports from smac planner */
-#include "smac_planner/a_star.hpp"
-#include "smac_planner/smoother.hpp"
-#include "smac_planner/costmap_downsampler.hpp"
-#include "nav_msgs/msg/occupancy_grid.hpp"
-#include "nav2_core/global_planner.hpp"
-#include "nav_msgs/msg/path.hpp"
-#include "nav2_costmap_2d/costmap_2d_ros.hpp"
-#include "nav2_costmap_2d/costmap_2d.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav2_core/global_planner.hpp"
+#include "nav2_costmap_2d/costmap_2d.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
+#include "nav2_msgs/msg/costmap.hpp"
 #include "nav2_util/lifecycle_node.hpp"
 #include "nav2_util/node_utils.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "smac_planner/a_star.hpp"
+#include "smac_planner/costmap_downsampler.hpp"
+#include "smac_planner/smoother.hpp"
 #include "tf2/utils.h"
 /* end of migrated imports from smac planner */
-#include <smac_planner/types.hpp>
 #include <smac_planner/constants.hpp>
+#include <smac_planner/types.hpp>
 
-namespace local_planning
-{
-class AStar : public TrajectoryGeneratorInterface
-{
-public:
-  AStar()
-  {
-    this->name = "A* algo";
-  }
+namespace local_planning {
+class AStar : public TrajectoryGeneratorInterface {
+  public:
+    AStar() { this->name = "A* algo"; }
 
-  void p_bridgeSMACConfigure();
+    void p_bridgeSMACConfigure();
 
-  nav_msgs::msg::Path computeTrajectory(const nav2_msgs::msg::Costmap::SharedPtr costmap,
-                                        const nav_msgs::msg::Odometry::SharedPtr odom,
-                                        const geometry_msgs::msg::PoseStamped::SharedPtr next_waypoint) override;
+    nav_msgs::msg::Path
+    computeTrajectory(const nav2_msgs::msg::Costmap::SharedPtr costmap,
+                      const nav_msgs::msg::Odometry::SharedPtr odom,
+                      const geometry_msgs::msg::PoseStamped::SharedPtr
+                          next_waypoint) override;
 
-  void configure(rclcpp_lifecycle::LifecycleNode::SharedPtr parent, std::shared_ptr<tf2_ros::Buffer> tf) override;
+    void configure(rclcpp_lifecycle::LifecycleNode::SharedPtr parent,
+                   std::shared_ptr<tf2_ros::Buffer> tf) override;
 
-  void cleanup() override;
+    void cleanup() override;
 
-  void activate() override;
+    void activate() override;
 
-  void deactivate();
+    void deactivate();
 
-private:
-  rclcpp_lifecycle::LifecycleNode::SharedPtr parent_node;
-  std::shared_ptr<tf2_ros::Buffer> tf_buffer;
+  private:
+    rclcpp_lifecycle::LifecycleNode::SharedPtr parent_node;
+    std::shared_ptr<tf2_ros::Buffer> tf_buffer;
 
-protected:
-  std::unique_ptr<smac_planner::AStarAlgorithm<smac_planner::NodeSE2>> _a_star;
-  std::unique_ptr<smac_planner::Smoother> _smoother;
-  rclcpp::Clock::SharedPtr _clock;
-  rclcpp::Logger _logger{rclcpp::get_logger("SmacPlanner")};
-  nav2_costmap_2d::Costmap2D * _costmap;
-  std::unique_ptr<smac_planner::CostmapDownsampler> _costmap_downsampler;
-  std::string _global_frame, _name;
-  float _tolerance;
-  int _downsampling_factor;
-  unsigned int _angle_quantizations;
-  double _angle_bin_size;
-  bool _downsample_costmap;
-  rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>::SharedPtr _raw_plan_publisher;
-  smac_planner::SmootherParams _smoother_params;
-  smac_planner::OptimizerParams _optimizer_params;
-  double _max_planning_time;
+  protected:
+    /* start migration from smac planner */
+    std::unique_ptr<smac_planner::AStarAlgorithm<smac_planner::NodeSE2>>
+        _a_star;
+    std::unique_ptr<smac_planner::Smoother> _smoother;
+    rclcpp::Clock::SharedPtr _clock;
+    rclcpp::Logger _logger{rclcpp::get_logger("A* algo")};
+    // TODO: add this back in
+    // std::unique_ptr<smac_planner::CostmapDownsampler> _costmap_downsampler;
+
+    std::string _global_frame, _name;
+    float _tolerance;
+    unsigned int _angle_quantizations;
+    double _angle_bin_size;
+    rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>::SharedPtr
+        _raw_plan_publisher;
+    smac_planner::SmootherParams _smoother_params;
+    smac_planner::OptimizerParams _optimizer_params;
+    double _max_planning_time;
+    /* end migration from smac planner*/
+
+    smac_planner::SearchInfo search_info;
+    smac_planner::MotionModel motion_model;
+    bool allow_unknown;
+    int max_iterations;
+    int max_on_approach_iterations = std::numeric_limits<int>::max();
+    int angle_quantizations;
+
+    bool smooth_path;
+    std::string motion_model_for_search;
 };
-}  // namespace local_planning
-#endif  // A_STAR_HPP_
+} // namespace local_planning
+#endif // A_STAR_HPP_
--- src/agent/src/planning/local_planning/local_planner_manager/include/local_planner_manager/local_planner_manager_node.hpp
+++ src/agent/src/planning/local_planning/local_planner_manager/include/local_planner_manager/local_planner_manager_node.hpp
@@ -1,111 +1,126 @@
 #ifndef LOCAL_PLANNER_MANAGER_NODE_HPP_
 #define LOCAL_PLANNER_MANAGER_NODE_HPP_
 
-#include "nav2_util/lifecycle_node.hpp"
+#include "control_interfaces/action/control.hpp"
 #include "geometry_msgs/msg/pose.hpp"
+#include "nav2_util/lifecycle_node.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "nav_msgs/msg/path.hpp"
+#include "planning_interfaces/action/trajectory_generation.hpp"
+#include "planning_interfaces/srv/trajectory_scoring.hpp"
 #include <nav2_msgs/msg/costmap_meta_data.hpp>
-#include <nav2_msgs/srv/get_costmap.hpp>
 #include <nav2_msgs/srv/clear_entire_costmap.hpp>
+#include <nav2_msgs/srv/get_costmap.hpp>
 #include <nav_msgs/msg/occupancy_grid.hpp>
 #include <trajectory_generator/trajectory_generator_ros.hpp>
 #include <trajectory_picker/trajectory_picker_ros.hpp>
-#include "planning_interfaces/action/trajectory_generation.hpp"
-#include "planning_interfaces/srv/trajectory_scoring.hpp"
-#include "control_interfaces/action/control.hpp"
 
-namespace local_planning
-{
-    class LocalPlannerManagerNode : public nav2_util::LifecycleNode
-    {
-        using TrajectoryGeneration = planning_interfaces::action::TrajectoryGeneration;
-        using GoalHandleTrajectoryGeneration = rclcpp_action::ClientGoalHandle<TrajectoryGeneration>;
+namespace local_planning {
+class LocalPlannerManagerNode : public nav2_util::LifecycleNode {
+    using TrajectoryGeneration =
+        planning_interfaces::action::TrajectoryGeneration;
+    using GoalHandleTrajectoryGeneration =
+        rclcpp_action::ClientGoalHandle<TrajectoryGeneration>;
 
-        using ControlAction = control_interfaces::action::Control;
-        using GoalHandleControlAction = rclcpp_action::ClientGoalHandle<ControlAction>;
-    public:
-        LocalPlannerManagerNode();
-        ~LocalPlannerManagerNode();
+    using ControlAction = control_interfaces::action::Control;
+    using GoalHandleControlAction =
+        rclcpp_action::ClientGoalHandle<ControlAction>;
 
-        void execute();
+  public:
+    LocalPlannerManagerNode();
+    ~LocalPlannerManagerNode();
 
+    void execute();
 
-    protected:
-        // implement the lifecycle interface
-        nav2_util::CallbackReturn on_configure(const rclcpp_lifecycle::State &state) override;
+  protected:
+    // implement the lifecycle interface
+    nav2_util::CallbackReturn
+    on_configure(const rclcpp_lifecycle::State &state) override;
 
-        nav2_util::CallbackReturn on_activate(const rclcpp_lifecycle::State &state) override;
+    nav2_util::CallbackReturn
+    on_activate(const rclcpp_lifecycle::State &state) override;
 
-        nav2_util::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &state) override;
+    nav2_util::CallbackReturn
+    on_deactivate(const rclcpp_lifecycle::State &state) override;
 
-        nav2_util::CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state) override;
+    nav2_util::CallbackReturn
+    on_cleanup(const rclcpp_lifecycle::State &state) override;
 
-        nav2_util::CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state) override;
+    nav2_util::CallbackReturn
+    on_shutdown(const rclcpp_lifecycle::State &state) override;
 
+    /* execution */
+    void p_execute();
+    rclcpp::TimerBase::SharedPtr execute_timer;
+    int num_execution = 0;
+    bool didReceiveAllMessages();
+    bool canExecute();
 
-        /* execution */
-        void p_execute();
-        rclcpp::TimerBase::SharedPtr execute_timer;
-        int num_execution = 0;
-        bool didReceiveAllMessages();
-        bool canExecute();
+    /* Waypoint */
+    std::shared_ptr<geometry_msgs::msg::PoseStamped> latest_waypoint_;
+    rclcpp::Subscription<geometry_msgs::msg::Pose>::ConstSharedPtr
+        next_waypoint_sub_;
+    std::mutex waypoint_mutex;
+    void onLatestWaypointReceived(geometry_msgs::msg::Pose::SharedPtr msg);
 
-        /* Waypoint */
-        std::shared_ptr<geometry_msgs::msg::PoseStamped> latest_waypoint_;
-        rclcpp::Subscription<geometry_msgs::msg::Pose>::ConstSharedPtr
-            next_waypoint_sub_;
-        std::mutex waypoint_mutex;
-        void onLatestWaypointReceived(geometry_msgs::msg::Pose::SharedPtr msg);
+    /* Odometry */
+    std::shared_ptr<nav_msgs::msg::Odometry> latest_odom;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::ConstSharedPtr odom_sub_;
+    std::mutex odom_mutex_;
+    void onLatestOdomReceived(nav_msgs::msg::Odometry::SharedPtr msg);
 
-        /* Odometry */
-        std::shared_ptr<nav_msgs::msg::Odometry> latest_odom;
-        rclcpp::Subscription<nav_msgs::msg::Odometry>::ConstSharedPtr
-            odom_sub_;
-        std::mutex odom_mutex_;
-        void onLatestOdomReceived(nav_msgs::msg::Odometry::SharedPtr msg);
+    /* Costmap */
+    std::shared_ptr<nav2_msgs::msg::Costmap> p_GetLatestCostmap();
+    std::shared_ptr<rclcpp::Node> costmap_node_;
+    rclcpp::Client<nav2_msgs::srv::GetCostmap>::SharedPtr costmap_client_;
+    void p_PrintCostMapInfo(const nav2_msgs::msg::Costmap::SharedPtr msg);
+    std::shared_ptr<nav2_msgs::msg::Costmap> latest_costmap_;
 
-        /* Costmap */
-        std::shared_ptr<nav2_msgs::msg::Costmap> p_GetLatestCostmap();
-        std::shared_ptr<rclcpp::Node>
-            costmap_node_;
-        rclcpp::Client<nav2_msgs::srv::GetCostmap>::SharedPtr costmap_client_;
-        void p_PrintCostMapInfo(const nav2_msgs::msg::Costmap::SharedPtr msg);
-        std::shared_ptr<nav2_msgs::msg::Costmap> latest_costmap_;
+    /* Trajectory Generator */
+    std::shared_ptr<local_planning::TrajectoryGeneratorROS>
+        trajectory_generator_node_;
+    std::unique_ptr<nav2_util::NodeThread> trajectory_generator_thread_;
+    rclcpp_action::Client<TrajectoryGeneration>::SharedPtr
+        trajectory_generator_client;
+    void trajectory_generator_goal_response_callback(
+        std::shared_future<GoalHandleTrajectoryGeneration::SharedPtr> future);
+    void trajectory_generator_feedback_callback(
+        GoalHandleTrajectoryGeneration::SharedPtr,
+        const std::shared_ptr<const TrajectoryGeneration::Feedback> feedback);
+    void trajectory_generator_result_callback(
+        const GoalHandleTrajectoryGeneration::WrappedResult &result);
+    void send_trajectory_generator_action(
+        const nav2_msgs::msg::Costmap::SharedPtr costmap,
+        const nav_msgs::msg::Odometry::SharedPtr odom,
+        const geometry_msgs::msg::PoseStamped::SharedPtr next_waypoint);
+    void register_generators();
+    int num_generator_execution = 0;
 
-        /* Trajectory Generator */
-        std::shared_ptr<local_planning::TrajectoryGeneratorROS>
-            trajectory_generator_node_;
-        std::unique_ptr<nav2_util::NodeThread> trajectory_generator_thread_;
-        rclcpp_action::Client<TrajectoryGeneration>::SharedPtr trajectory_generator_client;
-        void trajectory_generator_goal_response_callback(std::shared_future<GoalHandleTrajectoryGeneration::SharedPtr> future);
-        void trajectory_generator_feedback_callback(
-            GoalHandleTrajectoryGeneration::SharedPtr,
-            const std::shared_ptr<const TrajectoryGeneration::Feedback> feedback);
-        void trajectory_generator_result_callback(const GoalHandleTrajectoryGeneration::WrappedResult &result);
-        void send_trajectory_generator_action(
-            const nav2_msgs::msg::Costmap::SharedPtr costmap,
-            const nav_msgs::msg::Odometry::SharedPtr odom,
-            const geometry_msgs::msg::PoseStamped::SharedPtr next_waypoint);
-        void register_generators();
-        int num_generator_execution = 0;
+    /* Trajectory Picker */
+    std::shared_ptr<local_planning::TrajectoryPickerROS>
+        trajectory_picker_node_;
+    std::unique_ptr<nav2_util::NodeThread> trajectory_picker_thread_;
+    void on_best_trajectory_publication_received(
+        const planning_interfaces::msg::Trajectory::SharedPtr msg);
+    rclcpp::Subscription<planning_interfaces::msg::Trajectory>::SharedPtr
+        best_trajectory_subscriber_;
+    std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<
+        planning_interfaces::msg::Trajectory>>
+        possible_trajectory_publisher_;
 
-        /* Trajectory Picker */
-        std::shared_ptr<local_planning::TrajectoryPickerROS> trajectory_picker_node_;
-        std::unique_ptr<nav2_util::NodeThread> trajectory_picker_thread_;
-        void on_best_trajectory_publication_received(const planning_interfaces::msg::Trajectory::SharedPtr msg);
-        rclcpp::Subscription<planning_interfaces::msg::Trajectory>::SharedPtr best_trajectory_subscriber_;
-        std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<planning_interfaces::msg::Trajectory>> possible_trajectory_publisher_;
+    /* control client */
+    rclcpp_action::Client<ControlAction>::SharedPtr control_action_client_;
+    std::string controllerServerRoute;
 
-        /* control client */
-        rclcpp_action::Client<ControlAction>::SharedPtr control_action_client_;
-        std::string controllerServerRoute;
-        
-        void control_action_goal_response_callback(std::shared_future<GoalHandleControlAction::SharedPtr> future);
-        void control_action_feedback_callback(GoalHandleControlAction::SharedPtr future, const std::shared_ptr<const ControlAction::Feedback> feedback);
-        void control_action_result_callback(const GoalHandleControlAction::WrappedResult &result);
-        void control_send_goal(const nav_msgs::msg::Path::SharedPtr path, float target_spd);
-
-    };
-} // local_planning
+    void control_action_goal_response_callback(
+        std::shared_future<GoalHandleControlAction::SharedPtr> future);
+    void control_action_feedback_callback(
+        GoalHandleControlAction::SharedPtr future,
+        const std::shared_ptr<const ControlAction::Feedback> feedback);
+    void control_action_result_callback(
+        const GoalHandleControlAction::WrappedResult &result);
+    void control_send_goal(const nav_msgs::msg::Path::SharedPtr path,
+                           float target_spd);
+};
+} // namespace local_planning
 #endif
