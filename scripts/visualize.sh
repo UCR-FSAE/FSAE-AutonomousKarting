3e5461a72faa343b8f01013f9f78bcdc98bd8255
neil <neilkhadilkar23@berkeley.edu>
2023-07-31 23:47:36 -0700

    lite: donelite: done

--- scripts/visualize.sh
+++ scripts/visualize.sh
@@ -2,4 +2,4 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-ros2 run rviz2 rviz2 -d ./src/launches/launches/config/core/core.rviz
+ros2 run rviz2 rviz2 -d ./src/launches/launches/config/core/core.rviz --ros-args -p use_sim_time:=True
