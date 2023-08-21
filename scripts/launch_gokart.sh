bcfbd504d4d78e9c86b6dbb163059bbaa07bd0bd
Michael Wu <wuxiaohua1011@gmail.com>
2023-08-20 18:19:26 -0700

    lite: launch revamp

---
+++ scripts/launch_gokart.sh
@@ -0,0 +1,3 @@
+
+source install/setup.bash
+ros2 launch roar-indy-launches roar.launch.py gokart:=True params_file:=./src/launches/launches/config/gokart/config.yaml
--- scripts/launch_simulation_client.sh
+++ scripts/launch_simulation_client.sh
@@ -3,4 +3,4 @@
 # license that can be found in the LICENSE file.
 
 source install/setup.bash
-ros2 launch roar-indy-launches roar.launch.py manual_control:=True carla:=True
+ros2 launch roar-indy-launches roar.launch.py manual_control:=True carla:=True params_file:=./src/launches/launches/config/carla/config.yaml
