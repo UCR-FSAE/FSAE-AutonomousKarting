5b90c1cf81960b25d7847ac4b507eba3a0b03f15
Michael Wu <wuxiaohua1011@gmail.com>
2023-09-09 16:47:37 -0700

    repo and hokart upted

--- scripts/gokart/engage_control.sh
+++ scripts/gokart/engage_control.sh
@@ -1,2 +1,2 @@
 source install/setup.bash
-ros2 service call /controller/manager/safety_toggle roar_msgs/srv/ToggleControlSafetySwitch "{is_safety_on: True}"
+ros2 service call /roar/controller_manager/safety_toggle roar_msgs/srv/ToggleControlSafetySwitch "{is_safety_on: True}"
