c6a4393784cd7c731c9de27ec72db5e19468a97b
erichkw528 <97138813+erichkw528@users.noreply.github.com>
2023-11-03 00:46:38 -0700

    Repository Migration Script

---
+++ RepoMigrationScripts.py
@@ -0,0 +1,53 @@
+import os
+import subprocess
+from git import Repo
+from git.exc import GitCommandError
+from subprocess import CalledProcessError
+
+
+def migrate_repositories():
+    # Define the list of GitLab repository URLs
+    gitlab_repo_urls = [
+        "https://gitlab.com/roar-gokart/arduino_comm.git"
+        # Add URLs for your other GitLab repositories here
+    ]
+
+    # GitHub repository URL
+    github_repo_url = "https://github.com/UCR-FSAE/FSAE-AutonomousKarting.git"
+
+    # Clone each GitLab repository and push it to GitHub
+    for gitlab_url in gitlab_repo_urls:
+        # Extract the repository name from the URL
+        repo_name = gitlab_url.split("/")[-1].split(".git")[0]
+
+        try:
+            # Clone the GitLab repository
+            subprocess.run(["git", "clone", gitlab_url], check=True)
+
+            # Change directory to the cloned repository
+            os.chdir(repo_name)
+
+            # Add GitHub repository as a remote
+            subprocess.run(["git", "remote", "add", "github",
+                           github_repo_url], check=True)
+
+            # Push to GitHub
+            subprocess.run(["git", "push", "github", "develop"], check=True)
+
+        except (CalledProcessError, GitCommandError) as e:
+            print(
+                f"An error occurred during repository '{repo_name}' migration: {str(e)}")
+        finally:
+            # Change back to the original directory and clean up (delete cloned repo)
+            os.chdir("..")
+            subprocess.run(["rm", "-rf", repo_name], check=True)
+
+    print("Migration completed successfully.")
+
+
+def main():
+    migrate_repositories()
+
+
+if __name__ == "__main__":
+    main()
