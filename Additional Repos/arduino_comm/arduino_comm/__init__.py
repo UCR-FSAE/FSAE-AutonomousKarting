e6faf666184e0106dec54b6084171a8d21b766d9
erichkw528 <97138813+erichkw528@users.noreply.github.com>
2023-11-08 21:03:17 -0800

    test: checking push

--- Additional Repos/arduino_comm/arduino_comm/__init__.py
+++ Additional Repos/arduino_comm/arduino_comm/__init__.py
@@ -2,3 +2,6 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+
+# Edit by Eric: checking if we can commit / push changes
+# later, change the origin of the remote repo to test. 
